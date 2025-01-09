4f1f8343dded58090fce02365dd233072750c736
JoseAntonio1234t <joseantonioinstituto123@gmail.com>
2025-01-09 10:06:47 +0100

    5

--- Ej6.py
+++ Ej6.py
@@ -2,5 +2,7 @@ numero1 = int(input("Introduce el primer número:"))
 numero2 = int(input("Introduce el segundo número:"))
 if numero1 > numero2:
     print("El",numero1,"es el mayor.")
+elif numero1 == numero2:
+    print("Los números son iguales")
 else:
     print("El",numero2,"es el mayor.")
